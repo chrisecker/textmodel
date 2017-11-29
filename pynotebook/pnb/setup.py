db46e0e3d2dba9e063c913b6cb6ab5d26c44c914
C. Ecker <textmodelview@gmail.com>
2017-11-29 20:38:58 +0100

    Renamed pynb to pnb because there is already a pynb project on pypi

--- pynotebook/pnb/setup.py
+++ pynotebook/pnb/setup.py
@@ -9,16 +9,16 @@ from setuptools import setup
 with open('README') as file:
     long_description = file.read()
 
-setup(name='pynb',
-      version='0.2.2',
-      description = 'A beautiful and lighweight notebook for the python interpreter',
+setup(name='pnb',
+      version='0.2.3rc1',
+      description = 'A beautiful and lighweight notebook for the python interpreter.',
       long_description = long_description,
       author='C. Ecker',
       author_email='textmodelview@gmail.com',
-      url='https://pypi.python.org/pypi/pynotebook/',
+      url='https://github.com/chrisecker/textmodel/tree/master/pynotebook',
       license='BSD',
-      packages=['pynb/pynotebook', 'pynb/pynotebook/textmodel', 'pynb/pynotebook/wxtextview'],
-      scripts=['pynb/pynb'],
+      packages=['pnb/pynotebook', 'pnb/pynotebook/textmodel', 'pnb/pynotebook/wxtextview'],
+      scripts=['pnb/pnb'],
       platforms = ['any'],
      )
 
