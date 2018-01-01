5ee8bae266b018000e4eccaa742d6a71e1b3864f
C. Ecker <textmodelview@gmail.com>
2018-01-01 15:36:07 +0100

    Finalizing seperate releases for pnb and rnb.

--- pynotebook/pnb/setup.py
+++ pynotebook/pnb/setup.py
@@ -3,22 +3,29 @@
 
 from setuptools import setup
 
-# NOTE: if there is a problem with symlinks, update setuptools:
-# http://stackoverflow.com/questions/27459888/how-to-make-setuptools-follow-symlinks
 
 with open('README') as file:
     long_description = file.read()
 
 setup(name='pnb',
-      version='0.2.3rc1',
+      version='0.2.4rc1',
       description = 'A beautiful and lighweight notebook for the python interpreter.',
       long_description = long_description,
       author='C. Ecker',
       author_email='textmodelview@gmail.com',
       url='https://github.com/chrisecker/textmodel/tree/master/pynotebook',
       license='BSD',
-      packages=['pnb/pynotebook', 'pnb/pynotebook/textmodel', 'pnb/pynotebook/wxtextview'],
-      scripts=['pnb/pnb'],
-      platforms = ['any'],
+      scripts=['bin/pnb'],
+      install_requires = ['pynotebook > 0.2.3'],
+      platforms = "Linux, Mac OS X, Windows",
+      keywords = ['Interactive', 'Interpreter', 'Notebook', 'Shell', 'WXPython'],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+      ],      
      )
 
--- pynotebook/rnb/setup.py
+++ pynotebook/rnb/setup.py
@@ -3,25 +3,29 @@
 
 from setuptools import setup
 
-# NOTE: if there is a problem with symlinks, update setuptools:
-# http://stackoverflow.com/questions/27459888/how-to-make-setuptools-follow-symlinks
-
 with open('README') as file:
     long_description = file.read()
 
 setup(name='rnb',
-      version='0.2.3',
+      version='0.2.4rc1',
       description = 'An experimental notebook application for the r programming language.',
       long_description = long_description,
       author='C. Ecker',
       author_email='textmodelview@gmail.com',
       url='https://pypi.python.org/pypi/rnb/',
       license='BSD',
-      packages=['rnb/pynotebook', 'rnb/pynotebook/textmodel', 'rnb/pynotebook/wxtextview'],
-      scripts=['rnb/rnb'],
-      platforms = ['any'],
+      scripts=['bin/rnb'],
       install_requires=[
-          'rpy2', 'pygments'
+          'rpy2', 'pygments', 'pynotebook > 0.2.3'
       ],
-     )
+      platforms = "Linux, Mac OS X, Windows",
+      keywords = ['Interactive', 'Interpreter', 'Notebook', 'Shell', 'WXPython'],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+      ],
+)
 
