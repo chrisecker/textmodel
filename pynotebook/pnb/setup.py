e13810d30eb50c3a9697d853d5087db908f832bc
C. Ecker <textmodelview@gmail.com>
2019-08-11 12:14:33 +0200

    Release 0.2.5.3

--- pynotebook/pnb/setup.py
+++ pynotebook/pnb/setup.py
@@ -8,17 +8,19 @@ with open('README') as file:
     long_description = file.read()
 
 setup(name='pnb',
-      version='0.2.5.2',
-      description = 'A beautiful and lighweight notebook for the python interpreter.',
+      version='0.2.5.3',
+      description = 'A beautiful and lightweight notebook interface for the ' \
+                    'python interpreter.',
       long_description = long_description,
       author='C. Ecker',
       author_email='textmodelview@gmail.com',
       url='https://github.com/chrisecker/textmodel/tree/master/pynotebook',
       license='BSD',
       scripts=['bin/pnb'],
-      install_requires = ['pynotebook > 0.2.4'],
+      install_requires = ['pynotebook >= 0.2.5.3'],
       platforms = "Linux, Mac OS X, Windows",
-      keywords = ['Interactive', 'Interpreter', 'Notebook', 'Shell', 'WXPython'],
+      keywords = ['Interactive', 'Interpreter', 'Notebook', 'Shell',
+                  'WXPython'],
       classifiers = [
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
